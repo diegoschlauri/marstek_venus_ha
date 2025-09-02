3ae5cb30c8ba6fbc0098ec61d8282d689b176fae
diegoschlauri <diego.schlauri@gmail.com>
2025-09-02 16:34:38 +0200

    Create __init__.py

---
+++ custom_components/marstek_venus_ha/__init__.py
@@ -0,0 +1,34 @@
+"""The Marstek Venus HA integration."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import MarstekCoordinator
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Marstek Venus HA from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+
+    coordinator = MarstekCoordinator(hass, entry)
+    await coordinator.async_start_listening()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    # Reload entry when options are updated
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    coordinator = hass.data[DOMAIN].pop(entry.entry_id)
+    await coordinator.async_stop_listening()
+    return True
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload the config entry when options are changed."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
